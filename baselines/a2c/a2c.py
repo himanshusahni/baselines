updated calculation of Qu and Aohm. A few TODOs left and indexing needs to be fixed

baselines/a2c/a2c.py
@@ -30,8 +30,6 @@ class Model(object):
         A = tf.placeholder(tf.int32, [nbatch])     # Actions
         Q_U = tf.placeholder(tf.float32, [nbatch])
         A_OHM = tf.placeholder(tf.float32, [nbatch])  # Advantages for beta updates
-        ADV = tf.placeholder(tf.float32, [nbatch]) # Advantages
-        R = tf.placeholder(tf.float32, [nbatch])   # Rewards
         LR = tf.placeholder(tf.float32, [])        # Learning Rates
 
         step_model = policy(
@@ -77,14 +75,14 @@ class Model(object):
 
         lr = Scheduler(v=lr, nvalues=total_timesteps, schedule=lrschedule)
 
-        def train(obs, states, rewards, masks, actions, values):
-            advs = rewards - values
+        def train(obs, options, Qus, Aohms, actions):
             for step in range(len(obs)):
                 cur_lr = lr.value()
-            td_map = {train_model.X:obs, A:actions, ADV:advs, R:rewards, LR:cur_lr}
-            if states is not None:
-                td_map[train_model.S] = states
-                td_map[train_model.M] = masks
+            td_map = {train_model.X:obs, train_model.opt:options, Q_U:Qus, A_OHM:Aohms, A:actions}
+            # if states is not None:
+                # td_map[train_model.S] = states
+                # td_map[train_model.M] = masks
+            # TODO: Figure out - loss for option policies, beta, q_opt
             policy_loss, value_loss, policy_entropy, _ = sess.run(
                 [pg_loss, vf_loss, entropy, _train],
                 td_map
@@ -127,8 +125,8 @@ class Runner(object):
         self.gamma = gamma
         self.nsteps = nsteps
         self.states = model.initial_state
-        self.dones = [False for _ in range(nenv)]
-        self.opts = [None for _ in range(nenv)]
+        self.dones = [True for _ in range(nenv)]
+        self.opts = [0 for _ in range(nenv)]
         self.opt_dones = [True for _ in range(nenv)]
         self.opt_eps_start = 1.0
         self.opt_eps_end = 0.05
@@ -136,85 +134,87 @@ class Runner(object):
         self.opt_eps = self.opt_eps_start
 
     def run(self):
-        mb_obs, mb_rewards, mb_options, mb_actions, mb_opt_values, mb_betas, mb_opt_dones, mb_dones = [],[],[],[],[],[],[],[]
-        mb_states = self.states
+        mb_obs, mb_opt_values, mb_options, mb_actions, mb_betas, mb_dones, mb_rewards = [],[],[],[],[],[],[]
+        mb_options.append(np.copy(self.opts))
+        # mb_states = self.states
         for n in range(self.nsteps):
             # get option values
-            Qopt = self.model.Qopt(self.obs)
+            Qopt = self.model.step_model.Qopt(self.obs)
             # sample new options if ended last step
-            for n, done in enumerate(self.opts_done):
-                if done:
+            for i, opt_done in enumerate(self.opt_dones):
+                if opt_done:
                     # pick epsilon greedy
                     if np.random.uniform() < self.opt_eps:
-                        self.opts[n] = np.random.choice(np.arange(self.noptions))
+                        self.opts[i] = np.random.choice(np.arange(self.noptions))
                     else:
-                        self.opts[n] = np.random.choice(np.arange(self.noptions), p=Qopt[n])
-            actions, betas, opt_dones = self.model.step(self.obs, self.opts)
+                        self.opts[i] = np.argmax(Qopt[i])
+            actions, betas, opt_dones = self.model.step_model.step(self.obs, self.opts)
+            #TODO: update self.opt_dones
+            self.opt_dones = opt_dones
             mb_obs.append(np.copy(self.obs))
+            mb_opt_values.append(Qopt)
             mb_options.append(np.copy(self.opts))
             mb_actions.append(actions)
-            mb_opt_dones.append(opt_dones)
             mb_betas.append(betas)
-            mb_dones.append(self.dones)
-            mb_opt_values.append(Qopt)
+            mb_dones.append(np.copy(self.dones))
             obs, rewards, dones, _ = self.env.step(actions)
-            self.states = states
+            # self.states = states
             self.dones = dones
-            for n, done in enumerate(dones):
+            for i, done in enumerate(dones):
                 if done:
-                    self.obs[n] = self.obs[n]*0
-                    self.opts_done[n] = True
+                    self.obs[i] = self.obs[i]*0
+                    self.opt_dones[i] = True
             self.obs = obs
             mb_rewards.append(rewards)
             if self.opt_eps > self.opt_eps_end:
                 self.opt_eps -= (self.opt_eps_start - self.opt_eps_end)/self.opt_eps_steps
         mb_dones.append(self.dones)
-        #batch of steps to batch of rollouts
+        # mb_options -> (nenvs, nsteps+1) -> w_-1, w_0, w_1, w_2... w_nsteps-1
+        # mb_dones -> (nenvs, nsteps+1) -> d_-1, d_0, d_1, d_2... d_nsteps-1
+        # batch of steps to batch of rollouts
         mb_obs = np.asarray(mb_obs, dtype=np.uint8).swapaxes(1, 0).reshape(self.batch_ob_shape)
-        mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
-        mb_betas = np.asarray(mb_betas, dtype=np.float32).swapaxes(1, 0)
+        mb_opt_values = np.asarray(mb_opt_values, dtype=np.float32).swapaxes(1, 0)
         mb_options = np.asarray(mb_options, dtype=np.int32).swapaxes(1, 0)
         mb_actions = np.asarray(mb_actions, dtype=np.int32).swapaxes(1, 0)
-        mb_opt_values = np.asarray(mb_opt_values, dtype=np.float32).swapaxes(1, 0)
-        mb_opt_dones = np.asarray(mb_opt_dones, dtype=np.bool).swapaxes(1, 0)
+        mb_betas = np.asarray(mb_betas, dtype=np.float32).swapaxes(1, 0)
         mb_dones = np.asarray(mb_dones, dtype=np.bool).swapaxes(1, 0)
-        mb_masks = mb_dones[:, :-1]
-        mb_dones = mb_dones[:, 1:]
+        mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
+        # mb_masks = mb_dones[:, :-1]
 
-        #discount/bootstrap off option value fn
+        # discount/bootstrap off option value fn
         last_values = self.model.Qopt(self.obs).tolist()
         _, last_betas, _ = self.model.step(self.obs, self.opts)
-        for n, (rewards, dones, betas, options, opt_dones, opt_values, last_value, last_beta) in enumerate(zip(mb_rewards, mb_dones, mb_betas, mb_options, mb_opt_dones, mb_opt_values, last_values, last_betas)):
+        last_betas = last_betas.tolist()
+        mb_Qu = []
+        mb_Aohm = []
+        for n, (opt_values, options, betas, dones, rewards, last_value, last_beta) in enumerate(zip(mb_opt_values, mb_options,mb_betas, mb_dones, mb_rewards, last_values, last_betas)):
             rewards = rewards.tolist()
             dones = dones.tolist()
             betas = betas.tolist()
             options = options.tolist()
-            discounted = []
-            # last option value is bootstrapped
-            if dones[-1] == 0:
-                rewards += [last_value[options[-1]]]
-                dones += [0]
-                betas += [last_beta[options[-1]]]
-                options += [options[-1]]
-                opt_dones += [0]
+            # # last option value is bootstrapped
+            betas += [last_beta]
+            opt_values = np.concatenate((opt_values, np.expand_dims(last_values, axis=0)), axis=0)
+            # betas -> (nsteps+1,) -> b_0, b_1, .... b_nsteps
+            # opt_values -> (nsteps+1, noptions) -> Q_0, Q_1, .... Q_nsteps
             # reverse the option values
-            opt_values = np.flip(opt_values, axes=0)
-            r = 0
-            for reward, done, beta, opt, opt_done, opt_value in zip(rewards[::-1], dones[::-1], betas[::-1], options[::-1], opt_dones[::-1], opt_values):
-                # if option ended on this step, bootstrap Q again
-                if opt_dones:
-                    r = opt_value[opt]
-                r = reward + self.gamma*((1-beta[opt])*r + beta[opt]*np.amax(opt_value))
-
-
-            else:
-                rewards = discount_with_dones(rewards, dones, self.gamma)
-            mb_rewards[n] = rewards
-        mb_rewards = mb_rewards.flatten()
+            Qu = []
+            Aohm = []
+            for i in range(self.nsteps, 0, -1):
+                q = rewards[i-1] + (1-dones[i])*self.gamma*((1-betas[i])*opt_values[i,options[i-1]] + betas[i]*np.amax(opt_values[i]))
+                Qu.append(q)
+                a = opt_values[i-1, options[i-1]] - np.amax(opt_values[i-1])
+                Aohm.append(a)
+            mb_Qu.append(Qu)
+            mb_Aohm.append(Aohm)
+        mb_options = mb_options.flatten()
+        mb_Qu = mb_Qu.flatten()
+        mb_Aohm = mb_Aohm.flatten()
         mb_actions = mb_actions.flatten()
-        mb_values = mb_values.flatten()
-        mb_masks = mb_masks.flatten()
-        return mb_obs, mb_states, mb_rewards, mb_masks, mb_actions, mb_values
+        # mb_betas = mb_betas.flatten()
+        # mb_masks = mb_masks.flatten()
+        # TODO: comput Aohm
+        return mb_obs, mb_options, mb_Qu, mb_Aohm, mb_actions
 
 def learn(
         policy, env, seed, noptions=1, nsteps=5, total_timesteps=int(80e6),
@@ -244,9 +244,10 @@ def learn(
         lrschedule=lrschedule)
     sample_x = np.random.normal(size=(16,84,84,4))
     opt = np.random.randint(0,4,size=(16,))
-    q = model.sess.run([model.step_model.intra_pi_logits], feed_dict={model.step_model.X:sample_x, model.step_model.opt:opt})
-    print(q)
-    print(q[0].shape)
+    q = model.sess.run([model.step_model.betas, model.step_model.beta_logits], feed_dict={model.step_model.X:sample_x, model.step_model.opt:opt})
+    print(q[0][3])
+    print(q[1][3])
+    print(opt)
 
     exit()
 
@@ -257,8 +258,9 @@ def learn(
     nbatch = nenvs*nsteps
     tstart = time.time()
     for update in range(1, total_timesteps//nbatch+1):
-        obs, states, rewards, masks, actions, values = runner.run()
-        policy_loss, value_loss, policy_entropy = model.train(obs, states, rewards, masks, actions, values)
+        obs, options, Qus, Aohms, actions = runner.run()
+        #TODO: match the outputs
+        policy_loss, value_loss, policy_entropy = model.train(obs, options, Qus, Aohms, actions)
         nseconds = time.time()-tstart
         fps = int((update*nbatch)/nseconds)
         if update % log_interval == 0 or update == 1:

baselines/a2c/policies.py
@@ -124,14 +124,14 @@ class CnnPolicy(object):
         a0 = self.apd.sample()
         neglogp0 = -self.apd.log_prob(a0)
         # sample termination of options
-        beta = tf.sigmoid(beta_logits)
-        self.bpd = tf.distributions.Categorical(probs=beta)
-        beta0 = self.bpd.sample()
-        neglogpbeta0 = -self.bpd.log_prob(beta0)
+        beta0 = tf.sigmoid(beta_logits)
+        self.bpd = tf.distributions.Categorical(probs=beta0)
+        opt_done0 = self.bpd.sample()
+        neglogpbeta0 = -self.bpd.log_prob(opt_done)
 
         def step(ob, opt, *_args, **_kwargs):
-            a, opt_done = sess.run([a0, beta0], {X:ob, opt:opt})
-            return a, opt_done
+            a, beta, opt_done = sess.run([a0, beta0, opt_done0], {X:ob, opt:opt})
+            return a, beta, opt_done
 
         def Qopt(ob):
             '''Option values'''